964093bb0d4aa970621b9a0614565021d718ea96
Aleksey Kovalenko <alex2011ua@gmail.com>
2020-10-09 12:42:07 +0300

    all work

--- app.py
+++ app.py
@@ -1,68 +1,51 @@
-
 from flask import Flask,  render_template, request
 from data import *
 
 app = Flask(__name__)
 
-
-
-form = '''
-    <form class="auth-form" action="/signin" method="POST">
-    <div> Имя <input type="text" name="u_name" />  </div>
-    <div> Фамилия <input type="text" name="u_surname" /> </div>
-    <div> Почта <input type="email" name="u_mail" /> </div>
-    <div> Пароль <input type="password" name="u_pass" /> </div>
-    <div> Еще раз <input type="password" name="u_pass_again" /> </div>
-    <div> <input type="submit" value="Зарегистрироваться" />  </div>
-    </form>
-'''
-@app.route('/signin', methods = ["POST", 'GET'])
-def registration():
-    name = request.form.get('u_name')
-    sur = request.form.get('u_surname')
-    mail = request.form.get('u_mail')
-    pas_1 = request.form.get('u_pass')
-    pas_2 = request.form.get('u_pass_again')
-    if pas_1 == pas_2 and mail != None :
-        return f"Пользователь {name} {sur} с почтой {mail} зарегистрирован"
-    else:
-        return form
-
+@app.route('/flask/')
 @app.route('/')
 def index():
-
+    description = f"Всего доступно {len(tours)} туров. Воспользуйтесь сортировкой."
     return render_template('index.html',
                            title = title,
                            subtitle=subtitle,
                            description=description,
                            departures=departures,
                            tours=tours,
-
                            )
 
-
+@app.route('/flask/departures/<departure>/')
 @app.route('/departures/<departure>/')
 def departure(departure):
+    tour_is = {}
+    for item, tour in tours.items():
+        if tour['departure'] == departure:
+            tour_is[item] = tour
 
-
-    return render_template('departure.html', departure=departure,
+    description = {'count': len(tour_is),
+                   'min_price': min([x["price"] for x in tour_is.values()]),
+                   'max_price': max([x["price"] for x in tour_is.values()]),
+                   'min_night': min([x["nights"] for x in tour_is.values()]),
+                   'max_night': max([x["nights"] for x in tour_is.values()])
+                   }
+    return render_template('departure.html',
+                           departure=departure,
                            title = title,
-                           subtitle = subtitle,
                            description = description,
                            departures = departures,
-                           tours = tours,
+                           tours = tour_is,
                            )
 
-
+@app.route('/flask/tours/<id>/')
 @app.route('/tours/<id>/')
 def tour(id):
     t = tours[int(id)]
     tour1 = t['departure']
     departure = departures[tour1]
-    return render_template('tour.html', id=id,
+    return render_template('tour.html',
+                           id=id,
                            title = title,
-                           subtitle=subtitle,
-                           description=description,
                            departure=departure,
                            tour=tours[int(id)],
                            departures = departures,
